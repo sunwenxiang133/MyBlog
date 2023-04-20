e7762cbf5f724517e257b2a8b98387689ce25682
sun <sunwenxiang133@gmail.com>
2023-04-20 11:38:49 +0800

    fix(发现bug，使用日志中间件log4js尝试记录): 但愿天堂没有bug

--- MyBlogFront/src/components/SiderInfo.vue
+++ MyBlogFront/src/components/SiderInfo.vue
@@ -77,11 +77,15 @@
     </el-calendar>
     <div class="like-context card" :style="`${color};${mainBg}`">
       <i class="el-icon-alarm-clock" style="margin-right: 10px"></i
-      >年轻人，你怎么睡的着啊！
+      >你这个年龄段，能睡得着觉？有点出息没有？
     </div>
     <div class="like-context card" :style="`${color};${mainBg}`">
       <i class="el-icon-aim" style="margin-right: 10px"></i
-      >瞄准目标，坚定地走下去！
+      >经典的错误，标准的0分
+    </div>
+    <div class="like-context card" :style="`${color};${mainBg}`">
+      <i class="el-icon-alarm-clock" style="margin-right: 10px"></i
+      >晚上两点钟之前必须睡觉，早上多睡会儿，六点钟再起床，每天保持四个小时的充足睡眠
     </div>
   </div>
 </template>
